06d922316a1c7efb87cd5ff423573fe0c2c0783e
Mohammad Al Mouhtaseb <83731873+mohammad-Al-Mouhtaseb@users.noreply.github.com>
2024-05-30 21:18:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 from flask import Flask, jsonify
 import os
-
+from transformers import AutoProcessor, MusicgenForConditionalGeneration
+import requests, scipy, torch, threading, json
 app = Flask(__name__)
+
+
+model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
+processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+model.to(device)
+sampling_rate = model.config.audio_encoder.sampling_rate
 
 
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+@csrf_exempt
+def gen_fun(request):
+    inputs = processor(
+        text=['sad'],
+        padding=True,
+        return_tensors="pt",
+    )
+    audio_values = model.generate(**inputs.to(device), do_sample=True, guidance_scale=3, max_new_tokens=1202)
+    scipy.io.wavfile.write('s1', rate=sampling_rate, data=audio_values[0, 0].cpu().numpy())
+
+@app.route('/')
+def index():
+    t = threading.Thread(target=gen_fun, args=[request])
+    t.setDaemon(True)
+    t.start()
+    return jsonify({"res":"sucsess"})
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))

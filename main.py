61d19eb6acabba6bedbb254fa80e4388458642ac
Mohammad Al Mouhtaseb <83731873+mohammad-Al-Mouhtaseb@users.noreply.github.com>
2024-06-05 22:30:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,18 @@
 from flask import Flask, jsonify
 import os
-from transformers import AutoProcessor, MusicgenForConditionalGeneration
-import requests, scipy, torch
+import requests
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
-def index(): data = json.loads(request.body)
+def index():
+    data = json.loads(request.body)
     desc=data['desc']
-    doctor=data['doctor']
-    patient=data['patient']
-    type=data['type']
-
-    # music_path="sounds/music/"+type+"_"+desc+".flac"
-
     API_URL = "https://api-inference.huggingface.co/models/facebook/musicgen-small"
     headers = {"Authorization": "Bearer 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"}
     audio_bytes = {
         "inputs": desc
     }
     response = requests.post(API_URL, headers=headers, json=audio_bytes, timeout=120)
-    # music=Music.objects.create(doctor=doctor,patient=patient,music_path=music_path,type=type)
-    # music.save()
     return jsonify({"res":"sucsess"})
 
 if __name__ == '__main__':

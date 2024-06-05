029c80a84de29c5c3e81fff3d8726805e535ac2e
Mohammad Al Mouhtaseb <83731873+mohammad-Al-Mouhtaseb@users.noreply.github.com>
2024-06-05 22:55:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, redirect
 import os
-import requests
+import requests as req
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def index():
-    data = json.loads(request.body)
-    desc=data['desc']
+    desc=request.form['desc']
     API_URL = "https://api-inference.huggingface.co/models/facebook/musicgen-small"
     headers = {"Authorization": "Bearer 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"}
     audio_bytes = {
         "inputs": desc
     }
-    response = requests.post(API_URL, headers=headers, json=audio_bytes, timeout=120)
+    response = req.post(API_URL, headers=headers, json=audio_bytes, timeout=120)
     return jsonify({"res":"sucsess"})
 
 if __name__ == '__main__':

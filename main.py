e0a1e1f5df505cb6146d6e83adea617bfd903489
Mohammad Al Mouhtaseb <83731873+mohammad-Al-Mouhtaseb@users.noreply.github.com>
2024-06-05 23:06:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import os
 import requests as req
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST','GET'])
 def index():
+    print('start')
     desc=request.form['desc']
+    print(desc)
     API_URL = "https://api-inference.huggingface.co/models/facebook/musicgen-small"
     headers = {"Authorization": "Bearer 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"}
     audio_bytes = {
         "inputs": desc
     }
+    print('end')
     response = req.post(API_URL, headers=headers, json=audio_bytes, timeout=120)
     return jsonify({"res":"sucsess"})
 

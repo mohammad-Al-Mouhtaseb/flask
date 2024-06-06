4bf3ddd51fd0d0cbcd4aa96a0fc70c993d7ff6fa
Mohammad Al Mouhtaseb <83731873+mohammad-Al-Mouhtaseb@users.noreply.github.com>
2024-06-06 15:34:58 +0300

    Update main.py nnnn

--- main.py
+++ main.py
@@ -6,25 +6,12 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    try:
-        print('start')
-        desc=request.args.get('desc', '')
-        print(desc)
-        API_URL = "https://api-inference.huggingface.co/models/facebook/musicgen-small"
-        headers = {"Authorization": "Bearer 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"}
-        audio_bytes = {
-            "inputs": desc
-        }
-        print('1')
-        response = req.post(API_URL, headers=headers, json=audio_bytes)
-        print('2')
-        if response.status_code != 200:
-            return jsonify({"res":"Failed to get response from API"})
-        print('3')
-        return Response(response.content, mimetype='audio/flac')
-    except Exception as e:
-        print(e)
-        return jsonify({"res":"An error occurred"}), 500
+    API_URL = "https://api-inference.huggingface.co/models/facebook/musicgen-small"
+    headers = {"Authorization": "Bearer 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"}
+    audio_bytes = {
+        "inputs": "paly",
+    }
+    return Response(response.content)
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))

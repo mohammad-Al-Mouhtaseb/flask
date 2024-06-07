194bf42430f297f8e5769e063c5f1d73f002d3bc
Mohammad Al Mouhtaseb <83731873+mohammad-Al-Mouhtaseb@users.noreply.github.com>
2024-06-07 14:20:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    desc = request.args["desc"]
+    key = request.args["key"]
     API_URL = "https://api-inference.huggingface.co/models/facebook/musicgen-small"
-    headers = {"Authorization": "Bearer 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"}
+    headers = {"Authorization": "Bearer "+key}
     audio_bytes = {
-        "inputs": "paly",
+        "inputs": desc,
     }
     res = req.post(API_URL, headers=headers, json=audio_bytes)
-    print("1")
-    return Response(res.content)
+    return Response(res)#content
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
